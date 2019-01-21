479c154cf0105a09821ee04772c16241d84c3418
nikitahlukhov <46572110+nikitahlukhov@users.noreply.github.com>
2019-01-21 17:58:42 +0200

    1

---
+++ source/components/Feed/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+//Components
+import Post from 'components/Post';
+import Composer from 'components/Composer';
+import StatusBar from 'components/StatusBar';
+
+
+//Instruments
+import Styles from './styles.m.css';
+
+export default class Feed extends Component {
+    render () {
+        const {
+            avatar,
+            currentUserFirstName,
+        } = this.props;
+
+        return (
+            <section className = { Styles.feed }>
+                <StatusBar/>
+                <Composer 
+                avatar = { avatar }
+                currentUserFirstName = { currentUserFirstName }
+                />
+                <Post { ...this.props }/>
+            </section>
+        )
+    }
+}
